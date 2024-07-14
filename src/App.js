1e1831e27b23efda7c843875bb0bad7879939dc1
P541M <videna.psalmeleazar@gmail.com>
2024-07-14 18:16:34 -0400

    feats: love this new header

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <div>
       <Header />
-      {/* <Hero setWines={setWines} /> */}
+      <Hero setWines={setWines} />
       {/* <WineResults wines={wines} /> */}
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,20 +3,32 @@ import logo from "../assets/logo.png";
 
 const Header = () => {
   return (
-    <div className="w-full my-8 justify-center">
-      <div className="flex flex-row items-center justify-center">
-        <h1 className="text-4xl font-bold mr-4">SAVORY</h1>
-        <img
-          src={logo}
-          alt="SavorySips Logo"
-          style={{ width: "115px", height: "125px" }}
-        />
-        <h1 className="text-4xl font-bold ml-4">SIPS</h1>
+    <div className="w-full my-14 flex justify-center">
+      <div className="flex flex-row items-center">
+        <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold">SAV</h1>
+        <div
+          className="flex items-center"
+          style={{
+            width: "3em", // Default for small screens
+            height: "3.25em", // Default for small screens
+          }}
+        >
+          <img
+            src={logo}
+            alt="SavorySips Logo"
+            className="w-full h-full"
+            style={{
+              width: "100%",
+              height: "100%",
+              maxWidth: "4em", // Maximum width for large screens
+              maxHeight: "4.25em", // Maximum height for large screens
+            }}
+          />
+        </div>
+        <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold">RY SIPS</h1>
       </div>
     </div>
   );
 };
 
 export default Header;
-
-// header
