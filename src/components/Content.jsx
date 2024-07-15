8981caa42d74804c74c47337eedae8ef39f41653
P541M <videna.psalmeleazar@gmail.com>
2024-07-14 20:51:36 -0400

    feat: completed footer

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -49,68 +49,88 @@ const Content = ({ wines, setWines }) => {
   }, [newWines]);
 
   return (
-    <div
-      id="content"
-      className="w-full flex flex-col items-center justify-center min-h-screen bg-bg text-text fade-up"
-    >
-      <div className="relative w-full flex flex-col items-center justify-center body-font px-8 md:px-20 py-12 lg:py-20 bg-bgContrast min-h-screen">
-        <div className="relative text-center max-w-screen-lg mx-auto">
-          <p className="text-base sm:text-lg md:text-xl lg:text-2xl mb-6 fade-up">
-            A well-chosen wine can elevate a meal to new heights, enhancing the
-            flavors and creating a memorable dining experience. Sommeliers are
-            experts in wine pairing, using their extensive knowledge to
-            recommend wines that perfectly complement your dish, bringing out
-            the best in both the food and the wine.
-          </p>
-          <hr className="w-full border-t-2 border-primary my-8 fade-up" />
-          <p className="text-sm md:text-base lg:text-lg text-secondary italic mb-12 fade-up">
-            "Wine is sunlight, held together by water." — Galileo Galilei
-          </p>
-          <form
-            onSubmit={handleSubmit}
-            className="mt-8 flex flex-col items-center w-full max-w-md mx-auto fade-up"
-          >
-            <input
-              type="text"
-              value={dish}
-              onChange={(e) => setDish(e.target.value)}
-              placeholder="Enter your dish"
-              className="p-3 px-5 border rounded-lg mb-6 font-lora text-lg bg-bg w-full fade-up"
-            />
-            <button
-              type="submit"
-              className="p-3 px-6 bg-primary text-bg font-bold rounded-lg hover:bg-accent transition w-full fade-up"
+    <div className="flex flex-col min-h-screen bg-bgContrast">
+      <div
+        id="content"
+        className="w-full flex flex-col items-center justify-center flex-grow text-text fade-up"
+      >
+        <div className="relative w-full flex flex-col items-center justify-center body-font px-8 md:px-20 py-12 lg:py-20">
+          <div className="relative text-center max-w-screen-lg mx-auto">
+            <p className="text-base sm:text-lg md:text-xl lg:text-2xl mb-6 fade-up">
+              A well-chosen wine can elevate a meal to new heights, enhancing
+              the flavors and creating a memorable dining experience. Sommeliers
+              are experts in wine pairing, using their extensive knowledge to
+              recommend wines that perfectly complement your dish, bringing out
+              the best in both the food and the wine.
+            </p>
+            <hr className="w-full border-t-2 border-primary my-8 fade-up" />
+            <p className="text-sm md:text-base lg:text-lg text-secondary italic mb-12 fade-up">
+              "Wine is sunlight, held together by water." — Galileo Galilei
+            </p>
+            <form
+              onSubmit={handleSubmit}
+              className="mt-8 flex flex-col items-center w-full max-w-md mx-auto fade-up"
             >
-              Wine Pairing
-            </button>
-          </form>
-          {error && <p className="text-red-500 mt-4 fade-up">{error}</p>}
-          <div className="wine-results w-full flex flex-col items-center justify-center py-12">
-            <h2 className="text-2xl font-bold mb-4 fade-up">
-              Wine Recommendations
-            </h2>
-            {!wines.length && (
-              <p className="text-lg text-gray-600 mb-8 fade-up">
-                Wine pairings will appear here.
-              </p>
-            )}
-            <div className="w-full flex justify-center">
-              <div className="wine-list grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-6">
-                {wines.map((wine, index) => (
-                  <div
-                    key={index}
-                    className={`p-4 text-xl border rounded-lg bg-bg ${
-                      newWines.includes(wine) ? "fade-up" : ""
-                    }`}
-                  >
-                    <p className="font-bold font-josefin">
-                      {toTitleCase(wine)}
-                    </p>
-                  </div>
-                ))}
+              <input
+                type="text"
+                value={dish}
+                onChange={(e) => setDish(e.target.value)}
+                placeholder="Enter your dish"
+                className="p-3 px-5 border rounded-lg mb-6 font-lora text-lg bg-bg w-full fade-up"
+              />
+              <button
+                type="submit"
+                className="px-6 py-2 bg-primary text-bg font-bold rounded-lg hover:bg-accent transition fade-up"
+              >
+                Wine Pairing
+              </button>
+            </form>
+            {error && <p className="text-red-500 mt-4 fade-up">{error}</p>}
+            <div className="wine-results w-full flex flex-col items-center justify-center py-12">
+              <h2 className="text-2xl font-bold mb-4 fade-up">
+                Wine Recommendations
+              </h2>
+              {!wines.length && (
+                <p className="text-lg text-gray-600 mb-8 fade-up">
+                  Wine pairings will appear here.
+                </p>
+              )}
+              <div className="w-full flex justify-center">
+                <div className="wine-list grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4 sm:gap-6 md:gap-6 lg:gap-8 fade-up">
+                  {wines.map((wine, index) => (
+                    <div
+                      key={index}
+                      className={`p-3 sm:p-4 md:p-4 text-lg sm:text-xl md:text-xl border rounded-lg bg-bg ${
+                        newWines.includes(wine) ? "fade-up" : ""
+                      }`}
+                    >
+                      <p className="font-bold header-font">
+                        {toTitleCase(wine)}
+                      </p>
+                    </div>
+                  ))}
+                </div>
               </div>
             </div>
           </div>
+        </div>
+      </div>
+      <div className="w-full bg-bg text-text py-4 flex items-center justify-center">
+        <div className="text-center">
+          <p className="text-sm sm:text-base md:text-lg lg:text-xl body-font">
+            &copy; {new Date().getFullYear()} Savory Sips
+          </p>
+          <p className="text-sm body-font mt-1">
+            Website by{" "}
+            <a
+              href="https://github.com/P541M"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="underline text-primary hover:text-accent"
+            >
+              Psalm Eleazar
+            </a>
+          </p>
         </div>
       </div>
     </div>
